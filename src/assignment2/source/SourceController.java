371e25f24ef6760f4111b8e648c375b849312987
John Seymour <john.seymour787@gmail.com>
2021-10-13 12:35:07 +1100

    Initial commit for Movie xml file reading

--- src/assignment2/source/SourceController.java
+++ src/assignment2/source/SourceController.java
@@ -3,17 +3,47 @@ package assignment2.source;
 import assignment2.Movie;
 import assignment2.XMLController;
 import javafx.scene.Node;
+import javafx.stage.FileChooser;
+import org.xml.sax.SAXException;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class SourceController implements XMLController
 {
     private final SourceView view;
     private final ArrayList<Movie> model;
+    private File selectedFile;
 
     public SourceController(SourceView view, ArrayList<Movie> model)
     {
         this.view = view;
         this.model = model;
+
+        this.view.setSourceButtonHandler(event ->
+        {
+            FileChooser chooser= new FileChooser();
+            chooser.setTitle("Open File");
+            selectedFile = chooser.showOpenDialog(view.getViewWindow());
+        });
+
+
+
+        this.view.setLoadButtonHandler(event ->
+        {
+            try
+            {
+                SAXParser saxParser = SAXParserFactory.newInstance().newSAXParser();
+
+                MovieHandler handler = new MovieHandler(this.model);
+                saxParser.parse(selectedFile, handler);
+
+            }
+            catch (ParserConfigurationException | SAXException | IOException e) { e.printStackTrace(); }
+        });
     }
 
     public Node getViewNode()
--- src/assignment2/source/SourceView.java
+++ src/assignment2/source/SourceView.java
@@ -1,20 +1,49 @@
 package assignment2.source;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Node;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Window;
 
 public class SourceView
 {
     private final GridPane gridPane = new GridPane();
+    private final Button chooseSourceButton = new Button("Choose Source");
+    private final Button loadSourceButton = new Button("Load");
+    private final TextArea textArea = new TextArea();
 
     public SourceView()
     {
-        gridPane.addRow(0, new Label("This is a label"));
+        gridPane.addRow(0, chooseSourceButton);
+        gridPane.addRow(1, loadSourceButton);
+        gridPane.addRow(2, textArea);
     }
 
     public Node asNode()
     {
         return gridPane;
+    }
+
+    public Window getViewWindow()
+    {
+        return gridPane.getScene().getWindow();
+    }
+
+    public void setSourceButtonHandler(EventHandler<ActionEvent> handler)
+    {
+        chooseSourceButton.setOnAction(handler);
+    }
+
+    public void setLoadButtonHandler(EventHandler<ActionEvent> handler)
+    {
+        loadSourceButton.setOnAction(handler);
+    }
+
+    public void appendTextField(String toAppend)
+    {
+        textArea.appendText(toAppend);
     }
 }
