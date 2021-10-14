33fe1312addefcb5940715f7b0334014ce3a9f2e
Zichen1028 <simonwu0411@gmail.com>
2021-10-15 01:58:25 +1100

    Update SearchView.java

--- src/assignment2/search/SearchView.java
+++ src/assignment2/search/SearchView.java
@@ -13,18 +13,21 @@ public class SearchView {
     private double widthTextArea = 50;
     private double heightTextArea = 15;
     private final Button buttonEnter;
+    private final Label resultLabel;
 
     public SearchView() {
         gridPane = new GridPane();
         label = new Label("Please enter a keyword to search the movie.");
         inputTextField = new TextField();
-        buttonEnter =new Button("Search");
+        buttonEnter = new Button("Search");
+        resultLabel = new Label("the result of searching keyword will be displayed here");
 
         gridPane.addRow(0, label);
         gridPane.addRow(1, inputTextField);
         gridPane.addRow(1, buttonEnter);
         inputTextField.setPrefWidth(widthTextArea);
         inputTextField.setPrefHeight(heightTextArea);
+        gridPane.addRow(2, resultLabel);
     }
 
     public Node asNode()
