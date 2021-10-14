77ceb88fb95648620d4b5ecb0f8ad21b3a0d2f81
Zichen1028 <simonwu0411@gmail.com>
2021-10-15 01:48:03 +1100

    Update SearchView.java

--- src/assignment2/search/SearchView.java
+++ src/assignment2/search/SearchView.java
@@ -1,27 +1,35 @@
 package assignment2.search;
 
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.stage.Window;
 
 public class SearchView {
-    private final GridPane gridPane = new GridPane();
-    private final Label label = new Label("!");
+    private final GridPane gridPane;
+    private final Label label;
+    private final TextField inputTextField;
+    private double widthTextArea = 50;
+    private double heightTextArea = 15;
+    private final Button buttonEnter;
 
     public SearchView() {
+        gridPane = new GridPane();
+        label = new Label("Please enter a keyword to search the movie.");
+        inputTextField = new TextField();
+        buttonEnter =new Button("Search");
+
         gridPane.addRow(0, label);
+        gridPane.addRow(1, inputTextField);
+        gridPane.addRow(1, buttonEnter);
+        inputTextField.setPrefWidth(widthTextArea);
+        inputTextField.setPrefHeight(heightTextArea);
     }
 
     public Node asNode()
     {
         return gridPane;
     }
-
-    public Window getViewWindow()
-    {
-        return gridPane.getScene().getWindow();
-    }
-
 
 }
