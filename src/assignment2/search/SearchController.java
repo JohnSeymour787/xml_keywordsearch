c15bd8103ad5449c6279d0aa8bd0c641989c0eef
Zichen1028 <simonwu0411@gmail.com>
2021-10-16 03:52:17 +1100

    Update SearchController.java

--- src/assignment2/search/SearchController.java
+++ src/assignment2/search/SearchController.java
@@ -14,23 +14,25 @@ public class SearchController implements XMLController {
         this.view = view;
         this.model = model;
 
-        this.view.addSearchBtnListener(e -> {
-            List<String> keywordsListMovies = new ArrayList<>();
-            String inputString = this.view.getMoviesTitles();
-            String[] inputTitles = splitInputTitle(inputString);
-            for (String title : inputTitles) {
-                List<String> l = this.model.searchKeywords(title);
-                String lines[] = l.get(0).split("\\r?\\n");
-                List<String> li = new ArrayList<>();
-                for (int i = 1; i < lines.length; i++) {
-                    li.add(lines[i]);
+        try {
+            this.view.addSearchBtnListener(e -> {
+                List<String> keywordsListMovies = new ArrayList<>();
+                String inputString = this.view.getMoviesTitles();
+                String[] inputTitles = splitInputTitle(inputString);
+                for (String title : inputTitles) {
+                    List<String> l = this.model.searchKeywords(title);
+                    String lines[] = l.get(0).split("\\r?\\n");
+                    List<String> li = new ArrayList<>();
+                    for (int i = 1; i < lines.length; i++) {
+                        li.add(lines[i]);
+                    }
+                    keywordsListMovies.addAll(li);
                 }
-                keywordsListMovies.addAll(li);
-            }
-            this.view.setTextArea(keywordsListMovies);
-
-        });
-
+                this.view.setTextArea(keywordsListMovies);
+            });
+        } catch (Exception e) {
+            System.out.println("!!!!!");
+        }
     }
 
     private String[] splitInputTitle(String inputString) {
