9f29ad5188552d802027ba35fdfd48d76a12f072
Zichen1028 <simonwu0411@gmail.com>
2021-10-15 15:17:14 +1100

    Update SearchController.java

--- src/assignment2/search/SearchController.java
+++ src/assignment2/search/SearchController.java
@@ -1,17 +1,28 @@
 package assignment2.search;
 
 import assignment2.XMLController;
+import assignment2.source.SourceController;
 import javafx.scene.Node;
+
+import java.io.File;
 
 public class SearchController implements XMLController {
     private final SearchView view;
+    private File inputFile;
 
     public SearchController(SearchView view) {
         this.view = view;
+        this.inputFile = new File("imdb.xml");
+
+        this.view.addSearchBtnListener(e -> {
+            
+        });
+
     }
 
     public Node getViewNode()
     {
         return view.asNode();
     }
+
 }
