8387e470a693c7ef93f5cd9adc206aa86f84ad6e
github752020 <kikimimi@live.hk>
2021-10-16 15:56:00 +1100

    Updated visualisation view and controller, added model

--- src/assignment2/visualisation/VisualController.java
+++ src/assignment2/visualisation/VisualController.java
@@ -22,14 +22,12 @@ import javafx.collections.ObservableList;
 import javafx.geometry.Side;
 import javafx.scene.Group;
 import javafx.scene.Node;
-import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.PieChart;
 import javafx.scene.chart.PieChart.Data;
 import javafx.scene.chart.XYChart;
-import javafx.stage.Stage;
 
 public class VisualController implements XMLController {
 	private final VisualView view;
@@ -39,23 +37,8 @@ public class VisualController implements XMLController {
 			"extramarital-affair", "falling-down-stairs", "thompson-gun" };
 	int top = 3;
 
-	class Occurrence implements Comparable<Occurrence> {
-
-		int freq;
-		String keyword;
-
-		Occurrence(String keyword, int freq) {
-			this.keyword = keyword;
-			this.freq = freq;
-		}
-
-		public int compareTo(Occurrence e) {
-			return this.freq - e.freq;
-		}
-	}
-
 // Init the element list
-	ArrayList<Occurrence> occurs = new ArrayList<Occurrence>();
+	ArrayList<VisualModel> occurs = new ArrayList<VisualModel>();
 
 	public VisualController(VisualView view) {
 		this.view = view;
@@ -110,14 +93,14 @@ public class VisualController implements XMLController {
 			}
 
 			for (int i = 0; i < arr.length; i++) {
-				occurs.add(new Occurrence(arr[i], tab.get(arr[i])));
+				occurs.add(new VisualModel(arr[i], tab.get(arr[i])));
 			}
 
 			// Sort and print
 			Collections.sort(occurs);
 			Collections.reverse(occurs); // If you want reverse order
-			for (Occurrence occ : occurs) {
-				System.out.println(occ.keyword + " " + occ.freq);
+			for (VisualModel occ : occurs) {
+				System.out.println(occ.getKeyword() + " " + occ.getFreq());
 			}
 
 		} catch (SAXException e1) {
@@ -144,19 +127,13 @@ public class VisualController implements XMLController {
 				XYChart.Series<String, Integer> series = new XYChart.Series<>();
 
 				for (int i = 0; i < top; i++) {
-					series.getData().add(new XYChart.Data(occurs.get(i).keyword, occurs.get(i).freq));
+					series.getData().add(new XYChart.Data(occurs.get(i).getKeyword(), occurs.get(i).getFreq()));
 				}
 				series.setName("Class Frequency");
 
 				bar.getData().add(series);
 				Group root = new Group(bar);
-				// root.getChildren().add(bar);
-				Scene sc = new Scene(root, 500, 400);
-				Stage stage1 = new Stage();
-				stage1.setTitle("Bar Chart");
-				stage1.setScene(sc);
-				stage1.show();
-
+				this.view.setPane(root);
 			} catch (Exception e1) {
 				e1.printStackTrace();
 			}
@@ -169,7 +146,7 @@ public class VisualController implements XMLController {
 				ObservableList<Data> list = FXCollections.observableArrayList();
 
 				for (int i = 0; i < top; i++) {
-					list.add(new PieChart.Data(occurs.get(i).keyword, occurs.get(i).freq));
+					list.add(new PieChart.Data(occurs.get(i).getKeyword(), occurs.get(i).getFreq()));
 				}
 
 				PieChart pieChart = new PieChart();
@@ -180,11 +157,8 @@ public class VisualController implements XMLController {
 
 				Group root = new Group();
 				root.getChildren().add(pieChart);
-				Scene sc = new Scene(root, 500, 400);
-				Stage stage1 = new Stage();
-				stage1.setScene(sc);
-				stage1.setTitle("Pie Chart");
-				stage1.show();
+				this.view.setPane(root);
+				
 			} catch (Exception e1) {
 				e1.printStackTrace();
 			}
